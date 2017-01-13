d45294e6a07015a25193e446dd03f2e40838669e
윤장원 <yoonjw91@gmail.com>
2017-01-13 12:42:31 +0900

    안녕하세요 추가

--- HelloWorld/src/com/test/helloworld/HelloWorld.java
+++ HelloWorld/src/com/test/helloworld/HelloWorld.java
@@ -3,7 +3,7 @@ package com.test.helloworld;
 public class HelloWorld {
 	public static void main(String args[]){
 		System.out.println("Hello, World~!");
-		
+		System.out.println("안녕하세요.");
 		
 	}
 }
