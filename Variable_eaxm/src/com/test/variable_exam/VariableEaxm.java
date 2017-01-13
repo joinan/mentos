53aab768012f9e46b8fb91d49bc40830d3b176fe
Gunju <ragunju@naver.com>
2017-01-13 17:18:03 +0900

    퀴즈 폴더

---
+++ Variable_eaxm/src/com/test/variable_exam/VariableEaxm.java
@@ -0,0 +1,11 @@
+package com.test.variable_exam;
+
+public class VariableEaxm {
+	public static void main(String[] args) {
+		int value = 10;
+		int result = value+10;
+		
+		System.out.println(result);  
+		
+	}
+}
