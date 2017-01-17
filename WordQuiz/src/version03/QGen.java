f6a4d5804425e83c2eb7e9ca78de707230f312db
Gunju <ragunju@naver.com>
2017-01-17 17:57:13 +0900

    문제 생성기까지 개발

--- WordQuiz/src/version03/QGen.java
+++ WordQuiz/src/version03/QGen.java
@@ -1,8 +1,36 @@
 package version03;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class QGen {
-	String qSentence, qAns;
-	
+
+	Scanner sc = new Scanner(System.in);
+	String qSentence, qAns, qEaxm;
+	String inputSentence[] = new String[5];
+	String inputAns[] = new String[5];
+
+	ArrayList<String> qinfoList = new ArrayList<String>();
+	ArrayList<String> qList = new ArrayList<String>();
+	ArrayList<String> ansList = new ArrayList<String>();
+
+	public QGen() {
+		qinfoList.add("음식");
+		/*qinfoList.add("우리반 친구");
+		qinfoList.add("배우");
+		qinfoList.add("동물");*/
+
+		ansList.add("곱창");
+		/*ansList.add("이명우");
+		ansList.add("공유");
+		ansList.add("치타");*/
+
+		qList.add("육두쌍칸짜까치곱창동겹파자뽕면발");
+		/*qList.add("지원주명우수동환박성연인진정이롱");
+		qList.add("건제검박우빈공부유잔상든훤이만호");
+		qList.add("코염슴치호끼수지고랑원린가릴타문");*/
+	}
+
 	char[] qgenerator(int qNum) {
 		char[] arr = new char[16];
 		if (qNum == 1) {
@@ -17,8 +45,7 @@ public class QGen {
 		for (int i = 0; i < arr.length; i++) {
 			arr[i] = qSentence.charAt(i);
 		}
-		qNum++;
-		//System.out.println("qgenerator     "+Arrays.toString(arr));
+		// System.out.println("qgenerator "+Arrays.toString(arr));
 		return arr;
 	}
 
@@ -26,15 +53,48 @@ public class QGen {
 		int[] arr = new int[16];
 		int seed;
 		char temp;
-		
+
 		for (int i = 0; i < arr.length; i++) {
-			seed = (int) (Math.random() * 15)+1;
-			//System.out.println(seed);
+			seed = (int) (Math.random() * 15) + 1;
+			// System.out.println(seed);
 			temp = arrRandom[i];
 			arrRandom[i] = arrRandom[seed];
 			arrRandom[seed] = temp;
 		}
-		//System.out.println("qrandom     "+Arrays.toString(arrRandom));
+		// System.out.println("qrandom "+Arrays.toString(arrRandom));
 		return arrRandom;
+	}
+
+	char[] qgen(String str) {
+		char[] arr = new char[16];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = str.charAt(i);
+		}
+		// System.out.println("qgenerator "+Arrays.toString(arr));
+		return arr;
+	}
+
+	void inputQ() {
+		char[] arr = new char[16];
+		System.out.println("문제 입력");
+		qEaxm = sc.next();
+		qinfoList.add(qEaxm);
+		System.out.println("보기 입력");
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println((i + 1) + "번째 글자 입력");
+			arr[i] = sc.next().charAt(0);
+		}
+		qSentence = new String(arr);
+
+		qList.add(qSentence);
+		System.out.println("답 입력");
+		qAns = sc.next();
+		ansList.add(qAns);
+	}
+
+	void removeQ(int num) {
+		qinfoList.remove(num);
+		qList.remove(num);
+		ansList.remove(num);
 	}
 }
