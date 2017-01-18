3ca2fd6e088e4104ddf2d7517f0f0f54c106fd27
yoonjw91 <yoonjw91@gmail.com>
2017-01-18 09:56:01 +0900

    Quiz01 main 메소드 경량화 & scanner close

--- Variable_eaxm/src/com/test/variable_exam/Answer.java
+++ Variable_eaxm/src/com/test/variable_exam/Answer.java
@@ -7,7 +7,7 @@ public class Answer {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		Question question = new Question();
-		int menu;		
+		String menu;		
 		String trial;
 
 		while(true){
@@ -16,20 +16,16 @@ public class Answer {
 			System.out.println("│ 1.게임시작     	   │");
 			System.out.println("│ 2.게임종료     	   │");
 			System.out.println("└──────────────────┘");
-			menu = scan.nextInt();
-			if(menu == 1){
+			menu = scan.nextLine();
+			if(menu.equals("1")){
 				question.pick();
 				while(true){
 					System.out.println(question.printProblem());
-					System.out.println("┌─────────┐");		
+					System.out.println("┌─────────┐");
 					for(int i = 0; i < question.problem.size(); i++){
 						if(i % 4 == 0) System.out.print("│ ");
-						System.out.print(question.problem.get(i));
-						System.out.print(" ");
-						if(i % 4 == 3) {
-							System.out.print("│");
-							System.out.println();
-						}
+						System.out.print(question.problem.get(i) + " ");
+						if(i % 4 == 3) System.out.print("│\n");
 					}
 					System.out.println("└─────────┘");
 					System.out.print("정답은? : ");
@@ -52,15 +48,18 @@ public class Answer {
 							break;
 						}
 					}
+					scan2.close();
 				}
 			}
-			else if(menu == 2){
+			else if(menu.equals("2")){
 				System.out.println("<<게임 종료>>");
 				break;
 			}
 			else{
 				System.out.println("잘못 입력하셨습니다. 게임을 종료합니다.");
+				break;
 			}
+			scan.close();
 		}
 	}
 	
