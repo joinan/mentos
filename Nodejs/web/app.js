30aa683317804cb0c3d37ac935522103d9b79606
조인환 <choin0905@gmail.com>
2017-01-20 00:13:11 +0900

    Nodejs + Bootstrap 샘플 페이지 추가

---
+++ Nodejs/web/app.js
@@ -0,0 +1,62 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var index = require('./routes/index');
+var signin = require('./routes/signin');
+var login = require('./routes/login');
+var findpw = require('./routes/findpw');
+var main = require('./routes/main');
+var board = require('./routes/board');
+var movie = require('./routes/movie');
+var food = require('./routes/food');
+var center = require('./routes/center');
+var mypage = require('./routes/mypage');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/', main);
+app.use('/board', board);
+app.use('/signin', signin);
+app.use('/login', login);
+app.use('/findpw',findpw);
+app.use('/movie', movie);
+app.use('/food', food);
+app.use('/center', center);
+app.use('/mypage', mypage);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
